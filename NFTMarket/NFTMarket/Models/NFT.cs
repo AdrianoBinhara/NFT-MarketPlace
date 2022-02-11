4da29079fcd2a8c63ae2636c7e08b33c0bcf4ef7
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-02-10 23:30:35 -0300

    adicionando descricao

--- NFTMarket/NFTMarket/Models/NFT.cs
+++ NFTMarket/NFTMarket/Models/NFT.cs
@@ -20,10 +20,10 @@ namespace NFTMarket.Models
         {
             return new List<NFT>()
             {
-                new NFT{ Name = "Wave", Image = "Wave.png", Value = 0.46, Valuation = 3.99, Likes = 200},
-                new NFT{ Name = "Abstract Pink", Image = "AbstractPink.png", Value = 0.56, Valuation = 3.99, Likes = 129},
-                new NFT{ Name = "WaveBlue", Image = "WaveBlue.png", Value = 0.233, Valuation = 3.99, Likes = 156},
-                new NFT{ Name = "Abstract23", Image = "Abstract23.png", Value = 0.49, Valuation = 3.99, Likes =136}
+                new NFT{ Name = "Wave", Image = "Wave.png", Value = 0.46, Valuation = 3.99, Likes = 200, Description = "wav2 #5672"},
+                new NFT{ Name = "Abstract Pink", Image = "AbstractPink.png", Value = 0.56, Valuation = 3.99, Likes = 129, Description = "abstract #2538"},
+                new NFT{ Name = "WaveBlue", Image = "WaveBlue.png", Value = 0.233, Valuation = 3.99, Likes = 156, Description = "wavepi #5267"},
+                new NFT{ Name = "Abstract23", Image = "Abstract23.png", Value = 0.49, Valuation = 3.99, Likes =136, Description = "abstract #2038"}
             };
         }
 
@@ -32,5 +32,6 @@ namespace NFTMarket.Models
         public object Valuation { get; set; }
         public int Likes { get; set; }
         public string Image { get; set; }
+        public string Description { get; set; }
     }
 }
