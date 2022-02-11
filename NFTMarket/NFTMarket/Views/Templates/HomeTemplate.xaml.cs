46872b36b704ba9986d4019b471295f821dc8568
Adriano D'lucca Binhara <62717202+AdrianoBinhara@users.noreply.github.com>
2022-02-11 14:01:26 -0300

    adicionando template

---
+++ NFTMarket/NFTMarket/Views/Templates/HomeTemplate.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Sharpnado.MaterialFrame;
+using Xamarin.Forms;
+
+namespace NFTMarket.Views.Templates
+{
+    public partial class HomeTemplate : StackLayout
+    {
+        public HomeTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
